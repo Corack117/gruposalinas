652f70e6152a205adab0fb90e024d848cf1f10f6
Ordaz Romero Sergio <orcheko@gmail.com>
2022-05-20 16:51:30 +0000

    feat(BackEnd): create Worker model

--- BackEnd/applications/empleados/models.py
+++ BackEnd/applications/empleados/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from django.db import models, migrations
+from django.contrib.postgres.operations import UnaccentExtension
+from .manager import WorkerManager
 
 # Create your models here.
+class Migration(migrations.Migration):
+    operations = [
+        UnaccentExtension(),
+    ]
+
+class Worker(models.Model):
+    id_worker = models.IntegerField(blank=False, null=False, primary_key = True)
+    name = models.CharField(max_length=100, blank=False, null=False)
+    credito = models.IntegerField(blank=False, null=False)
+    ventaTotal = models.IntegerField(blank=False, null=False)
+    promedio = models.FloatField(blank=False, null=False)
+    
+    objects = WorkerManager()
+
+    class Meta:
+        ordering = ['name']
+        
