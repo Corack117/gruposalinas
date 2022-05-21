0cc88476d74d6823b8628f1dcb6570a2bfc4420f
Ordaz Romero Sergio <orcheko@gmail.com>
2022-05-21 07:30:45 +0000

    feat(FrontEnd): create modal-worker component

--- FrontEnd/src/app/main/main.module.ts
+++ FrontEnd/src/app/main/main.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrincipalComponent } from './principal/principal.component';
 import { MainRoutingModule } from './main-routing.module';
+import { ModalWorkerComponent } from './modal-worker/modal-worker.component';
 
 
 
 @NgModule({
   declarations: [
-    PrincipalComponent
+    PrincipalComponent,
+    ModalWorkerComponent
   ],
   imports: [
     CommonModule,
---
+++ FrontEnd/src/app/main/modal-worker/modal-worker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-worker',
+  templateUrl: './modal-worker.component.html',
+  styleUrls: ['./modal-worker.component.scss']
+})
+export class ModalWorkerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
