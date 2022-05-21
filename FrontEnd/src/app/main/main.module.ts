62bed0cf46e68c3dfeed25acd24caabd44ee8b72
Ordaz Romero Sergio <orcheko@gmail.com>
2022-05-21 07:31:11 +0000

    feat(FrontEnd): create filters component

--- FrontEnd/src/app/main/main.module.ts
+++ FrontEnd/src/app/main/main.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { PrincipalComponent } from './principal/principal.component';
 import { MainRoutingModule } from './main-routing.module';
 import { ModalWorkerComponent } from './modal-worker/modal-worker.component';
+import { FiltersComponent } from './filters/filters.component';
 
 
 
 @NgModule({
   declarations: [
     PrincipalComponent,
-    ModalWorkerComponent
+    ModalWorkerComponent,
+    FiltersComponent
   ],
   imports: [
     CommonModule,
