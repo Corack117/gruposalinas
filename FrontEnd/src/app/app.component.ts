9f0813f4de11cd298187a1dc0b85e5720f790534
Ordaz Romero Sergio <orcheko@gmail.com>
2022-05-21 04:26:39 +0000

    feat(FrontEnd): get csrf token

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiRequestsService } from './shared/api-requests.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FrontEnd';
+
+  constructor(private requestService: ApiRequestsService) {
+    this.requestService.getToken();
+  }
 }
