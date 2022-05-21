ebb8041e1b1eeae249655dce011977e752759cf0
Ordaz Romero Sergio <orcheko@gmail.com>
2022-05-21 08:34:59 +0000

    feat(FrontEnd): create worker-data service

---
+++ FrontEnd/src/app/shared/worker-data.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiRequestsService } from './api-requests.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class WorkerDataService {
+  public workers!: any;
+
+  constructor(private requestService: ApiRequestsService) { }
+
+  public getDataWorkers = (): Observable<any> => {
+    this.requestService.setWorkerUrl();
+    return this.requestService.get()
+  }
+
+  public filterDataWorkers = (filters: Filters): Observable<any> => {
+    this.requestService.setWorkerUrl();
+    return this.requestService.getWParams(filters)
+  }
+
+  public newWorker = (params: FormData) => {
+    this.requestService.setWorkerUrl();
+    params.append('info', 'new');
+    return this.requestService.post(params)
+  }
+
+  public updateWorker = (id: number, params: any) => {
+    this.requestService.setWorkerUrl(id);
+    params.append('info', 'update');
+    return this.requestService.post(params)
+  }
+
+  public deleteWorker = (id: number) => {
+    this.requestService.setWorkerUrl(id);
+    const params: FormData = new FormData();
+    params.append('info', 'delete');
+    return this.requestService.post(params);
+  }
+}
+
+interface Filters {
+  name: string
+}
