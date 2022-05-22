3f1f0b433566d7f34a4daee00a999eb16e5e9be2
Ordaz Romero Sergio <orcheko@gmail.com>
2022-05-22 18:29:50 +0000

    feat(BacktEnd): update WorkerSerializer

--- BackEnd/applications/empleados/serializers.py
+++ BackEnd/applications/empleados/serializers.py
@@ -11,4 +11,5 @@ class WorkerModelForm(forms.ModelForm):
 class WorkerSerializer(Serializer):
     
     def get_dump_object(self, obj):
+        self._current['id_worker'] = obj._get_pk_val()
         return self._current
