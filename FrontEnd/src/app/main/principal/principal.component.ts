c6e61e13aa9b0b9ea73a867def4fa96bf43ff10f
Ordaz Romero Sergio <orcheko@gmail.com>
2022-05-22 18:31:51 +0000

    feat(BacktEnd): update principal component

--- FrontEnd/src/app/main/principal/principal.component.ts
+++ FrontEnd/src/app/main/principal/principal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WorkerModalService } from 'src/app/shared/worker-modal.service';
 
 @Component({
   selector: 'app-principal',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrincipalComponent implements OnInit {
 
-  constructor() { }
+  constructor(public workerModal: WorkerModalService) {}
 
   ngOnInit(): void {
   }
 
+  public newWorker = () => {
+    this.workerModal.newWorkerModal();
+  }
 }
