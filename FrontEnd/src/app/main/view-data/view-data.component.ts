b4fffe3832c8580fc5cd31b7a4cd25436ad481cd
Ordaz Romero Sergio <orcheko@gmail.com>
2022-05-22 18:31:26 +0000

    feat(BacktEnd): update viewl-data component

--- FrontEnd/src/app/main/view-data/view-data.component.ts
+++ FrontEnd/src/app/main/view-data/view-data.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
+import { WorkerDataService } from 'src/app/shared/worker-data.service';
+import { WorkerModalService } from 'src/app/shared/worker-modal.service';
 
 @Component({
   selector: 'app-view-data',
@@ -6,10 +9,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-data.component.scss']
 })
 export class ViewDataComponent implements OnInit {
+  public isShowModal: boolean = false;
+  public worker!: any;
+  private orderedBy: string = 'lastName';
+  private isAscendant: boolean = true;
 
-  constructor() { }
+  @ViewChild('modal') modal!: ElementRef;
+  @ViewChild('swalSuccess') swalSuccess!: SwalComponent;
+  @ViewChild('swalError') swalError!: SwalComponent;
+
+  constructor(
+    public workerData: WorkerDataService,
+    public workerModal: WorkerModalService
+  ) {
+    this.workerData.getDataWorkers()
+    .subscribe((res: any) => {
+      this.workerData.workers = res;
+    }, () => this.swalError.fire())
+  }
 
   ngOnInit(): void {
   }
 
+  public orderBy = (key: string) => {
+    if (this.orderedBy != key) {
+      this.orderedBy = key;
+      this.isAscendant = true;
+    }
+    if(this.isAscendant)
+      this.workerData.workers.sort((a: any, b: any) => {
+        return a[key] > b[key];
+      })
+    else
+      this.workerData.workers.sort((a: any, b: any) => {
+        return a[key] < b[key];
+      })
+    this.isAscendant = !this.isAscendant;
+  }
+
+  public showModal = (id: number) => {
+    this.worker = this.workerData.workers.find((worker: any) => {
+      return worker.id === id
+    })
+
+    this.isShowModal = !this.isShowModal;
+  }
+
+  public hideModal = (e: Event) => {
+    const modal = e.target as HTMLElement;
+    if (modal.classList.contains('modal-data'))
+      this.isShowModal = !this.isShowModal;
+  }
+
+  public deleteWorker = (id: number) => {
+    this.workerData.deleteWorker(id).subscribe(() => {
+      this.swalSuccess.fire();
+      this.workerData.workers = this.workerData.workers.filter(
+        (worker: any) => worker.id != id
+      );
+    },
+      () => this.swalError.fire()
+    )
+  }
 }
